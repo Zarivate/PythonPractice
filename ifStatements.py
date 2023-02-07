61a4db72f968a2e7c60cab259ba3cb19bb9887c4
Zarivate <Zarivate1@gmail.com>
2023-02-06 23:59:26 -0600

    if statement testing

---
+++ ifStatements.py
@@ -0,0 +1,13 @@
+# If statements, work very similar to other languages
+
+age = int(input("How old are you?: "))
+
+# In Python you need to watch out for the indentation after if statements that signal where if condition starts
+if age >= 18:
+    print("Congrats, you now pay taxes!")
+# elif is shorthand for else if
+elif age <= 0:
+    print("How are you even answering this? Did you lie??????")
+# else works the same in Python where it's the last option once the others have been exauhsted
+else:
+    print("You are not a legal adult yet... lucky.")
