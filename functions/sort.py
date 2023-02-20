0c0694eeccc1b669018e4070d5a5bf6ced0d1fc7
Zarivate <Zarivate1@gmail.com>
2023-02-19 23:59:41 -0600

    Sorting

---
+++ functions/sort.py
@@ -0,0 +1,5 @@
+# Sort technically has two variations, the sort() method and the sort() function
+# The method is used for lists while the function is used for iterables, does the same
+# thing as it would in other languages. Where it sorts a series of data in a variety of ways
+
+example = [""]
