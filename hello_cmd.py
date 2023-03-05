6500a06f8f046e3e3b1c6fa00e03c57b9178911b
Zarivate <Zarivate1@gmail.com>
2023-03-04 23:59:03 -0600

    Practice with command line running

---
+++ hello_cmd.py
@@ -0,0 +1,8 @@
+# Simple file to test out running Python scripts from the command line
+# Have to invoke both the interpreter + the script in question
+
+print("Howdy, this is hopefully coming from the cmd line!")
+
+name = input("What's your name?: ")
+
+print("Hello " + name)
